259884a291c8673894ba72c30ecc7d0ea931f9ff
Luca Ban <mesqueeb@users.noreply.github.com>
2022-01-25 12:03:17 +0900

    chore: build

--- dist/index.cjs
+++ dist/index.cjs
@@ -4,27 +4,6 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var isWhat = require('is-what');
 
-/*! *****************************************************************************
-Copyright (c) Microsoft Corporation.
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-***************************************************************************** */
-
-function __spreadArray(to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-}
-
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 function concatArrays(originVal, newVal) {
     if (isWhat.isArray(originVal) && isWhat.isArray(newVal)) {
@@ -35,7 +14,7 @@ function concatArrays(originVal, newVal) {
 }
 
 function assignProp(carry, key, newVal, originalObject) {
-    var propType = {}.propertyIsEnumerable.call(originalObject, key)
+    const propType = {}.propertyIsEnumerable.call(originalObject, key)
         ? 'enumerable'
         : 'nonenumerable';
     if (propType === 'enumerable')
@@ -54,12 +33,12 @@ function mergeRecursively(origin, newComer, compareFn) {
     if (!isWhat.isPlainObject(newComer))
         return newComer;
     // define newObject to merge all values upon
-    var newObject = {};
+    let newObject = {};
     if (isWhat.isPlainObject(origin)) {
-        var props_1 = Object.getOwnPropertyNames(origin);
-        var symbols_1 = Object.getOwnPropertySymbols(origin);
-        newObject = __spreadArray(__spreadArray([], props_1), symbols_1).reduce(function (carry, key) {
-            var targetVal = origin[key];
+        const props = Object.getOwnPropertyNames(origin);
+        const symbols = Object.getOwnPropertySymbols(origin);
+        newObject = [...props, ...symbols].reduce((carry, key) => {
+            const targetVal = origin[key];
             if ((!isWhat.isSymbol(key) && !Object.getOwnPropertyNames(newComer).includes(key)) ||
                 (isWhat.isSymbol(key) && !Object.getOwnPropertySymbols(newComer).includes(key))) {
                 assignProp(carry, key, targetVal, origin);
@@ -68,17 +47,17 @@ function mergeRecursively(origin, newComer, compareFn) {
         }, {});
     }
     // newObject has all properties that newComer hasn't
-    var props = Object.getOwnPropertyNames(newComer);
-    var symbols = Object.getOwnPropertySymbols(newComer);
-    var result = __spreadArray(__spreadArray([], props), symbols).reduce(function (carry, key) {
+    const props = Object.getOwnPropertyNames(newComer);
+    const symbols = Object.getOwnPropertySymbols(newComer);
+    const result = [...props, ...symbols].reduce((carry, key) => {
         // re-define the origin and newComer as targetVal and newVal
-        var newVal = newComer[key];
-        var targetVal = isWhat.isPlainObject(origin) ? origin[key] : undefined;
+        let newVal = newComer[key];
+        const targetVal = isWhat.isPlainObject(origin) ? origin[key] : undefined;
         // When newVal is an object do the merge recursively
         if (targetVal !== undefined && isWhat.isPlainObject(newVal)) {
             newVal = mergeRecursively(targetVal, newVal, compareFn);
         }
-        var propToAssign = compareFn ? compareFn(targetVal, newVal, key) : newVal;
+        const propToAssign = compareFn ? compareFn(targetVal, newVal, key) : newVal;
         assignProp(carry, key, propToAssign, newComer);
         return carry;
     }, newObject);
@@ -91,30 +70,18 @@ function mergeRecursively(origin, newComer, compareFn) {
  * @param object
  * @param otherObjects
  */
-function merge(object) {
-    var otherObjects = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        otherObjects[_i - 1] = arguments[_i];
-    }
-    return otherObjects.reduce(function (result, newComer) {
+function merge(object, ...otherObjects) {
+    return otherObjects.reduce((result, newComer) => {
         return mergeRecursively(result, newComer);
     }, object);
 }
-function mergeAndCompare(compareFn, object) {
-    var otherObjects = [];
-    for (var _i = 2; _i < arguments.length; _i++) {
-        otherObjects[_i - 2] = arguments[_i];
-    }
-    return otherObjects.reduce(function (result, newComer) {
+function mergeAndCompare(compareFn, object, ...otherObjects) {
+    return otherObjects.reduce((result, newComer) => {
         return mergeRecursively(result, newComer, compareFn);
     }, object);
 }
-function mergeAndConcat(object) {
-    var otherObjects = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        otherObjects[_i - 1] = arguments[_i];
-    }
-    return otherObjects.reduce(function (result, newComer) {
+function mergeAndConcat(object, ...otherObjects) {
+    return otherObjects.reduce((result, newComer) => {
         return mergeRecursively(result, newComer, concatArrays);
     }, object);
 }
